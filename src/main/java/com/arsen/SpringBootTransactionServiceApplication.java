c2ed58e5fbfba48498948982d21f2b4a45135a5b
Arsen <arsproger@gmail.com>
2023-04-23 17:28:10 +0600

    init

---
+++ src/main/java/com/arsen/SpringBootTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTransactionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTransactionServiceApplication.class, args);
+    }
+
+}
